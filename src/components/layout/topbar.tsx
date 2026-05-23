7dd44cdae926ad1d68a693691e5dc177cf6d42fb
Breno <brenosg2310@gmail.com>
2026-05-23 19:42:21 -0300

    style(topbar): Ajust topbar layout

--- src/components/layout/topbar.tsx
+++ src/components/layout/topbar.tsx
@@ -1,11 +1,11 @@
 import type { ReactElement, ReactNode } from "react"
 
 import { cn } from "@/lib/utils"
-import type { NavLinkProps } from "react-router"
+import { type NavLinkProps } from "react-router"
 import { Book } from "lucide-react"
 
 type Props = {
-  navComponents: ReactElement<NavLinkProps>[]
+  navComponents?: ReactElement<NavLinkProps>[]
   className?: string
   actions?: ReactNode
 }
@@ -14,16 +14,16 @@ function Topbar({ navComponents = [], className, actions }: Props) {
   return (
     <div
       className={cn(
-        "flex flex-col gap-3 px-6 py-4 sm:flex-row sm:items-center sm:justify-between",
+        "flex gap-3 px-10 py-4 justify-between sm:flex-row sm:items-center",
         className,
       )}
     >
-      <div className="flex items-center gap-2 text-gray-700 font-bold text-xl">
+      <div className="flex items-center gap-2 text-primary font-bold text-xl">
         <Book />
         Monitoria
       </div>
 
-      <nav className="space-y-1">
+      <nav className="hidden lg:block space-y-1">
         <ul className="flex justify-between gap-7">
           {navComponents &&
             navComponents.map((component) => (
