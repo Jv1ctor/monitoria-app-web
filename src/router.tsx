2a804aeeeffafb395b7e2058adc5f64c25b9bfc8
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-05-23 21:52:04 -0300

    feat(router): add public layout for index page

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router"
 import { DesignSystem } from "./pages/DesignSystem"
 import { Example } from "./pages/Example"
+import { PublicLayout } from "./components/layout/public-layout"
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Example />,
+    element: <PublicLayout />,
+    children: [{ index: true, element: <Example /> }],
     loader: () => console.log("Executa sempre quando carrega uma page"),
   },
   {
