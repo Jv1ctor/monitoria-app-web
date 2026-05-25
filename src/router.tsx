187769648d814723168846d94280a3d8fa75d694
Breno <brenosg2310@gmail.com>
2026-05-25 04:44:12 -0300

    feat(publicPage): Add PublicPage page route

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from "react-router"
 import { DesignSystem } from "./pages/DesignSystem"
-import { Example } from "./pages/Example"
 import { PublicLayout } from "./components/layout/public-layout"
+import { PublicPage } from "./pages/PublicPage/PublicPage"
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <PublicLayout />,
-    children: [{ index: true, element: <Example /> }],
+    children: [{ index: true, element: <PublicPage /> }],
     loader: () => console.log("Executa sempre quando carrega uma page"),
   },
   {
