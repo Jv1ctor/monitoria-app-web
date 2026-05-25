67c2d438ba3c00098fdccbd6f298e90c0e80bc4f
Breno <brenosg2310@gmail.com>
2026-05-25 04:42:52 -0300

    feat(publicPage): Add page and components of PublicPage

---
+++ src/pages/PublicPage/components/HeroPublic.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button"
+import { NavLink } from "react-router"
+
+function HeroPublic() {
+    return (
+        <div className="flex flex-col my-20 px-8 gap-5 items-center justify-center w-full">
+            <h1 className="text-5xl text-center font-bold">
+                Conectando Alunos e Monitores
+            </h1>
+            <p className="max-w-2xl text-center">
+                Encontre monitores qualificados ou ofereça suas habilidades para ajudar outros estudantes a alcançarem seus objetivos.
+            </p>
+            <div className="flex items-center justify-center gap-2">
+                <NavLink to='/ds'>
+                    <Button>Encontrar Monitor</Button>
+                </NavLink>
+                <NavLink to='/ds'>
+                    <Button variant='outline'>Sou monitor</Button>
+                </NavLink>
+            </div>
+        </div>
+    )
+}
+
+export { HeroPublic }
